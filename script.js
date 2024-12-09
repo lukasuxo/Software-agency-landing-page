2a82e674ec829c5674e643b83a660629a55655ec
lukasuxo <lukasukhiashvili06@gmail.com>
2024-12-09 20:39:24 +0400

    START/1/RESPONSIV DARCHA

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//Software agency landing page//
+function openModal(modalId) {
+  document.getElementById(modalId).classList.add("active");
+  document.getElementById("sidebarClose").classList.add("active");
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).classList.remove("active");
+  document.getElementById("sidebarClose").classList.remove("active");
+}
+
+function closeAllModals() {
+  document
+    .querySelectorAll(".modal")
+    .forEach((modal) => modal.classList.remove("active"));
+  document.getElementById("sidebarClose").classList.remove("active");
+}
+
+function handleForm(modalId) {
+  alert("თქვენ წარმატებით გაიარეთ რეგისტრაცია❤️✅");
+  closeModal(modalId);
+  return false;
+}
