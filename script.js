1593db40c8902242f9288c66881d8b3ac2396a6d
berokvernadze <bero.kvernadze.21@gmail.com>
2024-12-25 19:18:19 +0400

    third page

--- script.js
+++ script.js
@@ -22,6 +22,40 @@ function handleForm(modalId) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  const imageElement = document.getElementById("ITImgHover");
+
+
+  const hoverEffects = [
+      {
+          id: "BestQuality",
+          imageSrc: "./images/quality.png",
+      },
+      {
+          id: "AffordablePrices",
+          imageSrc: "./images/ptice.jpg",
+      },
+      {
+          id: "CustemerServices",
+          imageSrc: "./images/services.jpg",
+      },
+  ];
+
+
+  hoverEffects.forEach(({ id, imageSrc }) => {
+      const element = document.getElementById(id);
+      if (element) {
+          element.addEventListener("mouseenter", () => {
+              imageElement.src = imageSrc;
+          });
+
+          element.addEventListener("mouseleave", () => {
+              imageElement.src = "./images/js_pictur1.jpg";
+          });
+      }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
   const showVideoButton = document.querySelector(".show-video");
   const videoContainer = document.querySelector(".video-container");
   const closeVideoButton = document.querySelector(".close-video");
